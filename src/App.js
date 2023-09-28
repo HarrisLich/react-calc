4ec802247ec97af33d85aa939755b94df18df4fa
HarrisLich <112398044+HarrisLich@users.noreply.github.com>
2023-09-28 09:44:41 -0400

    j

--- src/App.js
+++ src/App.js
@@ -114,61 +114,61 @@ function App() {
           <h1 className="md:text-4xl sm:text-2xl">{value}</h1>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleOperatorClick("C")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl font-bold">C</button>
+          <button onClick={() => handleOperatorClick("C")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl font-bold">C</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleOperatorClick("+/-")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl font-bold">+/-</button>
+          <button onClick={() => handleOperatorClick("+/-")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl font-bold">+/-</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
           <button className="rounded-full md:w-16 md:h-16 bg-gray-300 md:text-2xl sm:w-14 sm:h-14 w-10 h-10 font-bold">%</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleOperatorClick("/")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-orange-400 md:text-2xl text-white font-bold">/</button>
+          <button onClick={() => handleOperatorClick("/")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-orange-300 bg-orange-400 md:text-2xl text-white font-bold">/</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick("7")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">7</button>
+          <button onClick={() => handleNumberClick("7")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">7</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick("8")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">8</button>
+          <button onClick={() => handleNumberClick("8")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">8</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick("9")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">9</button>
+          <button onClick={() => handleNumberClick("9")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">9</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleOperatorClick("*")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-orange-400 md:text-2xl text-white font-bold">X</button>
+          <button onClick={() => handleOperatorClick("*")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-orange-300 bg-orange-400 md:text-2xl text-white font-bold">X</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick("4")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">4</button>
+          <button onClick={() => handleNumberClick("4")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">4</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick("5")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">5</button>
+          <button onClick={() => handleNumberClick("5")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">5</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick("6")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">6</button>
+          <button onClick={() => handleNumberClick("6")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">6</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleOperatorClick("-")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-orange-400 md:text-2xl text-white font-bold">-</button>
+          <button onClick={() => handleOperatorClick("-")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-orange-300 bg-orange-400 md:text-2xl text-white font-bold">-</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick("1")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">1</button>
+          <button onClick={() => handleNumberClick("1")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">1</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick("2")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">2</button>
+          <button onClick={() => handleNumberClick("2")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">2</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick("3")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">3</button>
+          <button onClick={() => handleNumberClick("3")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">3</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleOperatorClick("+")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-orange-400 md:text-2xl text-white font-bold">+</button>
+          <button onClick={() => handleOperatorClick("+")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-orange-300 bg-orange-400 md:text-2xl text-white font-bold">+</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2 col-span-2">
-          <button onClick={() => handleNumberClick("0")} className="rounded-full w-[88%] sm:h-14 py-1 px-3 md:w-[88%] md:h-16 bg-gray-300 md:text-2xl flex items-center justify-start md:px-6">0</button>
+          <button onClick={() => handleNumberClick("0")} className="rounded-full w-[88%] sm:h-14 py-1 px-3 md:w-[88%] md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl flex items-center justify-start md:px-6">0</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleNumberClick(".")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-gray-300 md:text-2xl">.</button>
+          <button onClick={() => handleNumberClick(".")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 hover:bg-gray-200 bg-gray-300 md:text-2xl">.</button>
         </div>
         <div className="w-full h-full flex justify-center items-center md:py-2">
-          <button onClick={() => handleOperatorClick("=")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-orange-400 md:text-2xl text-white font-bold">=</button>
+          <button onClick={() => handleOperatorClick("=")} className="rounded-full sm:w-14 sm:h-14 w-10 h-10 md:w-16 md:h-16 bg-orange-400 hover:bg-orange-300 md:text-2xl text-white font-bold">=</button>
         </div>
       </div>
     </div>
